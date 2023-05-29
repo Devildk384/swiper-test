45cdc0582766e6b038f0b2dda5353716b71a2a2a
Durgesh384 <durgesh@techrefic.in>
2023-05-29 14:31:53 +0530

    remove css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/zoom";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+// import "swiper/css";
+// import "swiper/css/zoom";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
 
 // import "./styles.css";
 
