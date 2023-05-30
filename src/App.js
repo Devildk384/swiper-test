c21df49dc9dd6c185d6d546fda23844a999f8d80
Durgesh384 <durgesh@techrefic.in>
2023-05-30 13:16:51 +0530

    update image url

--- src/App.js
+++ src/App.js
@@ -31,47 +31,47 @@ export default function App() {
       >
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            <img src="https://d1820pa95bipcd.cloudfront.net/files/b556cc3f85ac4c1baa4b214736ffc22c/content" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+            <img src="https://d1820pa95bipcd.cloudfront.net/files/c50b3e44a5634874bfdec41cce412fdc/content" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+            <img src="https://d1820pa95bipcd.cloudfront.net/files/ec5f9ba1ec58428290f00798f03b19ad/content" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+            <img src="https://d1820pa95bipcd.cloudfront.net/files/44ae66fc97db467c8a195b7e3963e5a1/content" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+            <img src="https://d1820pa95bipcd.cloudfront.net/files/c2684d5adcb34af2a4299f4a88cb3b59/content" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+            <img src="https://d1820pa95bipcd.cloudfront.net/files/c39a5e01c0e24e96b36260aec35b0269/content" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+            <img src="https://d1820pa95bipcd.cloudfront.net/files/dec0e16beadd4556a6b02900ea3d0b96/content" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+            <img src="https://d1820pa95bipcd.cloudfront.net/files/a011dcc4777e4c88b074ded8d85934c0/content" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="swiper-zoom-container">
-            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+            <img src="https://d1820pa95bipcd.cloudfront.net/files/4643498d9fa64de588b49875cdeb3b28/content" />
           </div>
         </SwiperSlide>
       </Swiper>
