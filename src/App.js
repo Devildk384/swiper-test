fbe390668ef0de5c3a36e743b667f29578187bfe
Durgesh384 <durgesh@techrefic.in>
2023-06-13 17:53:50 +0530

    latest push

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-// import "swiper/css/zoom";
+import "swiper/css/zoom";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-import "./styles.css";
+// import "./styles.css";
 
 // import required modules
 import { Zoom, Navigation, Pagination } from "swiper";
