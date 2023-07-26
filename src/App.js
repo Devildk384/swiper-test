358027acb26e6bbbbe6ed2bec1528ad03caf2a1c
Durgesh384 <durgesh@techrefic.in>
2023-07-26 11:03:07 +0530

    ddd

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ export default function App() {
           <LazyLoadImage
             alt={"image.alt"}
             height={600}
-            src={"https://d1820pa95bipcd.cloudfront.net/files/b556cc3f85ac4c1baa4b214736ffc22c/content"} // use normal <img> attributes as props
+            src={"https://d1820pa95bipcd.cloudfront.net/files/b556cc3f85ac4c1baa4b214736ffc22c/content?width=426"} // use normal <img> attributes as props
             width={400} />
           </div>
         </SwiperSlide>
@@ -47,9 +47,9 @@ export default function App() {
           <LazyLoadImage
             alt={"image.alt"}
             height={600}
-            src={"https://d1820pa95bipcd.cloudfront.net/files/c50b3e44a5634874bfdec41cce412fdc/content"} // use normal <img> attributes as props
+            src={"https://d1820pa95bipcd.cloudfront.net/files/c50b3e44a5634874bfdec41cce412fdc/content?width=426"} // use normal <img> attributes as props
             width={400} />
-            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/c50b3e44a5634874bfdec41cce412fdc/content" /> */}
+            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/c50b3e44a5634874bfdec41cce412fdc/content?width=426" /> */}
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -57,9 +57,9 @@ export default function App() {
           <LazyLoadImage
             alt={"image.alt"}
             height={600}
-            src={"https://d1820pa95bipcd.cloudfront.net/files/ec5f9ba1ec58428290f00798f03b19ad/content"} // use normal <img> attributes as props
+            src={"https://d1820pa95bipcd.cloudfront.net/files/ec5f9ba1ec58428290f00798f03b19ad/content?width=426"} // use normal <img> attributes as props
             width={400} />
-            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/ec5f9ba1ec58428290f00798f03b19ad/content" /> */}
+            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/ec5f9ba1ec58428290f00798f03b19ad/content?width=426" /> */}
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -67,9 +67,9 @@ export default function App() {
           <LazyLoadImage
             alt={"image.alt"}
             height={600}
-            src={"https://d1820pa95bipcd.cloudfront.net/files/44ae66fc97db467c8a195b7e3963e5a1/content"} // use normal <img> attributes as props
+            src={"https://d1820pa95bipcd.cloudfront.net/files/44ae66fc97db467c8a195b7e3963e5a1/content?width=426"} // use normal <img> attributes as props
             width={400} />
-            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/44ae66fc97db467c8a195b7e3963e5a1/content" /> */}
+            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/44ae66fc97db467c8a195b7e3963e5a1/content?width=426" /> */}
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -77,9 +77,9 @@ export default function App() {
           <LazyLoadImage
             alt={"image.alt"}
             height={600}
-            src={"https://d1820pa95bipcd.cloudfront.net/files/c2684d5adcb34af2a4299f4a88cb3b59/content"} // use normal <img> attributes as props
+            src={"https://d1820pa95bipcd.cloudfront.net/files/c2684d5adcb34af2a4299f4a88cb3b59/content?width=426"} // use normal <img> attributes as props
             width={400} />
-            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/c2684d5adcb34af2a4299f4a88cb3b59/content" /> */}
+            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/c2684d5adcb34af2a4299f4a88cb3b59/content?width=426" /> */}
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -87,9 +87,9 @@ export default function App() {
           <LazyLoadImage
             alt={"image.alt"}
             height={600}
-            src={"https://d1820pa95bipcd.cloudfront.net/files/c39a5e01c0e24e96b36260aec35b0269/content"} // use normal <img> attributes as props
+            src={"https://d1820pa95bipcd.cloudfront.net/files/c39a5e01c0e24e96b36260aec35b0269/content?width=426"} // use normal <img> attributes as props
             width={400} />
-            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/c39a5e01c0e24e96b36260aec35b0269/content" /> */}
+            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/c39a5e01c0e24e96b36260aec35b0269/content?width=426" /> */}
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -97,9 +97,9 @@ export default function App() {
           <LazyLoadImage
             alt={"image.alt"}
             height={600}
-            src={"https://d1820pa95bipcd.cloudfront.net/files/dec0e16beadd4556a6b02900ea3d0b96/content"} // use normal <img> attributes as props
+            src={"https://d1820pa95bipcd.cloudfront.net/files/dec0e16beadd4556a6b02900ea3d0b96/content?width=426"} // use normal <img> attributes as props
             width={400} />
-            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/dec0e16beadd4556a6b02900ea3d0b96/content" /> */}
+            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/dec0e16beadd4556a6b02900ea3d0b96/content?width=426" /> */}
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -107,9 +107,9 @@ export default function App() {
           <LazyLoadImage
             alt={"image.alt"}
             height={600}
-            src={"https://d1820pa95bipcd.cloudfront.net/files/a011dcc4777e4c88b074ded8d85934c0/content"} // use normal <img> attributes as props
+            src={"https://d1820pa95bipcd.cloudfront.net/files/a011dcc4777e4c88b074ded8d85934c0/content?width=426"} // use normal <img> attributes as props
             width={400} />
-            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/a011dcc4777e4c88b074ded8d85934c0/content" /> */}
+            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/a011dcc4777e4c88b074ded8d85934c0/content?width=426" /> */}
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -117,9 +117,9 @@ export default function App() {
           <LazyLoadImage
             alt={"image.alt"}
             height={600}
-            src={"https://d1820pa95bipcd.cloudfront.net/files/4643498d9fa64de588b49875cdeb3b28/content"} // use normal <img> attributes as props
+            src={"https://d1820pa95bipcd.cloudfront.net/files/4643498d9fa64de588b49875cdeb3b28/content?width=426"} // use normal <img> attributes as props
             width={400} />
-            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/4643498d9fa64de588b49875cdeb3b28/content" /> */}
+            {/* <img src="https://d1820pa95bipcd.cloudfront.net/files/4643498d9fa64de588b49875cdeb3b28/content?width=426" /> */}
           </div>
         </SwiperSlide>
       </Swiper>
