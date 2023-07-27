33dc2e03d08470057bacd37f0c6821e111d8c3bb
Durgesh384 <durgesh@techrefic.in>
2023-07-27 13:53:31 +0530

    hhh

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ export default function App() {
           <div className="swiper-zoom-container">
           <LazyLoadImage
             alt={"image.alt"}
-            // height={600}
+            height={600}
             src={"https://d1820pa95bipcd.cloudfront.net/files/b556cc3f85ac4c1baa4b214736ffc22c/content?width=426"} // use normal <img> attributes as props
             width={400} 
             />
@@ -47,7 +47,7 @@ export default function App() {
           <div className="swiper-zoom-container">
           <LazyLoadImage
             alt={"image.alt"}
-            // height={600}
+            height={600}
             src={"https://d1820pa95bipcd.cloudfront.net/files/c50b3e44a5634874bfdec41cce412fdc/content?width=426"} // use normal <img> attributes as props
             width={400} 
             />
@@ -58,7 +58,7 @@ export default function App() {
           <div className="swiper-zoom-container">
           <LazyLoadImage
             alt={"image.alt"}
-            // height={600}
+            height={600}
             src={"https://d1820pa95bipcd.cloudfront.net/files/ec5f9ba1ec58428290f00798f03b19ad/content?width=426"} // use normal <img> attributes as props
             width={400} 
             />
@@ -69,7 +69,7 @@ export default function App() {
           <div className="swiper-zoom-container">
           <LazyLoadImage
             alt={"image.alt"}
-            // height={600}
+            height={600}
             src={"https://d1820pa95bipcd.cloudfront.net/files/44ae66fc97db467c8a195b7e3963e5a1/content?width=426"} // use normal <img> attributes as props
             width={400} 
             />
@@ -80,7 +80,7 @@ export default function App() {
           <div className="swiper-zoom-container">
           <LazyLoadImage
             alt={"image.alt"}
-            // height={600}
+            height={600}
             src={"https://d1820pa95bipcd.cloudfront.net/files/c2684d5adcb34af2a4299f4a88cb3b59/content?width=426"} // use normal <img> attributes as props
             width={400} 
             />
@@ -91,7 +91,7 @@ export default function App() {
           <div className="swiper-zoom-container">
           <LazyLoadImage
             alt={"image.alt"}
-            // height={600}
+            height={600}
             src={"https://d1820pa95bipcd.cloudfront.net/files/c39a5e01c0e24e96b36260aec35b0269/content?width=426"} // use normal <img> attributes as props
             width={400} 
             />
@@ -102,7 +102,7 @@ export default function App() {
           <div className="swiper-zoom-container">
           <LazyLoadImage
             alt={"image.alt"}
-            // height={600}
+            height={600}
             src={"https://d1820pa95bipcd.cloudfront.net/files/dec0e16beadd4556a6b02900ea3d0b96/content?width=426"} // use normal <img> attributes as props
             width={400} 
             />
@@ -113,7 +113,7 @@ export default function App() {
           <div className="swiper-zoom-container">
           <LazyLoadImage
             alt={"image.alt"}
-            // height={600}
+            height={600}
             src={"https://d1820pa95bipcd.cloudfront.net/files/a011dcc4777e4c88b074ded8d85934c0/content?width=426"} // use normal <img> attributes as props
             width={400} 
             />
@@ -124,7 +124,7 @@ export default function App() {
           <div className="swiper-zoom-container">
           <LazyLoadImage
             alt={"image.alt"}
-            // height={600}
+            height={600}
             src={"https://d1820pa95bipcd.cloudfront.net/files/4643498d9fa64de588b49875cdeb3b28/content?width=426"} // use normal <img> attributes as props
             width={400} 
             />
